3145e33dc3b31c20cf6882e4f488e06a1705bce3
Yosuke.Tanaka <y3tanaka@gmail.com>
2022-12-17 09:03:59 +0900

    Update sensor input logic.etc

--- src/lambdashifter2.cpp
+++ src/lambdashifter2.cpp
@@ -18,19 +18,21 @@ This program performs the following functions
 #include <AsyncElegantOTA.h>
 #include <ESP32Ping.h>
 #include "esp_adc_cal.h"
+#include "driver/adc.h"
+#include "driver/dac.h"
 #define FORMAT_SPIFFS_IF_FAILED true
 
 ///// Firm version
-const String firm_version = "2.0.10";
+const String firm_version = "2.0.12";
 
 ///// WiFi Config /////
 
-const char ap_ssid[] = "LS20-AP0026";
-const char ap_pass[] = "Q6bZwk8z5K"; 
+const char ap_ssid[] = "LS20-AP0000";
+const char ap_pass[] = "0123456789";
 const IPAddress ap_ip(10, 1, 1, 1);
 const IPAddress ap_subnet(255, 255, 255, 0);
 const IPAddress dns(8, 8, 8, 8);
-AsyncWebServer server(80); // 
+AsyncWebServer server(80); //
 
 String ssid;
 String wifi_pass;
@@ -58,7 +60,7 @@ const int O2OUT_PIN2 = 26;
 const int CODE_PIN1 = 18;
 const int CODE_PIN2 = 19;
 
-// Variable for rewrite web pages. 
+// Variable for rewrite web pages.
 String file_version;
 String af_value;
 String map_value;
@@ -78,15 +80,19 @@ int code_value;
 int counter;
 int reset_code = 0;
 
-///// Function for rewrite web pages 
+esp_adc_cal_characteristics_t adcChar;
+
+///// Function for rewrite web pages
 String processor(const String &var)
 {
   if (var == "AF_STATE")
   {
-    if (af_value == "0.01"){
+    if (af_value == "0.01")
+    {
       return "Disable";
     }
-    else{
+    else
+    {
       return af_value;
     }
   }
@@ -200,7 +206,7 @@ String processor(const String &var)
     }
   }
 
-// Switch secondary o2sensor graph
+  // Switch secondary o2sensor graph
   if (var == "SECONDARY_OFF1" && secondary_value == "OFF")
   {
     return "<!--";
@@ -224,7 +230,6 @@ String processor(const String &var)
   {
     return secondary_value;
   }
-
 
   // For WiFi configration page.
   if (var == "SSID")
@@ -365,45 +370,29 @@ void start_ap()
   WiFi.disconnect(true);
   WiFi.mode(WIFI_OFF);
   delay(100);
-  WiFi.softAP(ap_ssid, ap_pass);              
-  delay(100);                                 
-  WiFi.softAPConfig(ap_ip, ap_ip, ap_subnet); 
-  IPAddress myIP = WiFi.softAPIP();           
-      Serial.println("AP Connected");
-    Serial.println(myIP);
-}
-
-// General ADC calibration Function
-float ReadVoltage(byte ADC_PIN) {
-  float calibration  = 1.000; // Adjust for ultimate accuracy when input is measured using an accurate DVM, if reading too high then use e.g. 0.99, too low use 1.01
-  float vref = 1100;
-  esp_adc_cal_characteristics_t adc_chars;
-  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  vref = adc_chars.vref; // Obtain the device ADC reference voltage
-  // offset 10mV around 28,  
-  return ((analogRead(ADC_PIN) + 285)/ 4095.0) * 1.270 * (1100 / vref) * calibration;  // ESP by design reference voltage in mV
+  WiFi.softAP(ap_ssid, ap_pass);
+  delay(100);
+  WiFi.softAPConfig(ap_ip, ap_ip, ap_subnet);
+  IPAddress myIP = WiFi.softAPIP();
 }
 
 ///////////////////////////// Execution Part //////////////////////////////
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("");
-
+  setCpuFrequencyMhz(80);
   pinMode(O2OUT_PIN1, OUTPUT);
   pinMode(O2OUT_PIN2, OUTPUT);
   pinMode(CODE_PIN1, OUTPUT);
   pinMode(CODE_PIN2, OUTPUT);
-  analogSetAttenuation(ADC_2_5db);  
-  analogSetWidth(10);
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_2_5); // GPIO35
+  adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_2_5); // GPIO32
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_2_5, ADC_WIDTH_BIT_12, 1100, &adcChar);
+  dac_output_enable(DAC_CHANNEL_1);
+  dac_output_enable(DAC_CHANNEL_2);
 
-
-  // SPIFFS File read/write 
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
+  // SPIFFS File read/write
+  SPIFFS.begin(true);
 
   ///// Initialize log file
   SPIFFS.remove("/ls-oat_log.csv");
@@ -458,11 +447,7 @@ void setup()
   const IPAddress gateway(gateway_data0, gateway_data1, gateway_data2, gateway_data3);
   fr2.close();
 
- 
- // Serial.println(ssid);
- // Serial.println(String(wifi_pass));
-
- // Depending on the result of the file read, the output value for the coding plug is changed.
+  // Depending on the result of the file read, the output value for the coding plug is changed.
   if (code_value == 1)
   {
     digitalWrite(CODE_PIN1, LOW);
@@ -481,14 +466,12 @@ void setup()
 
   // Try WiFi Client mode.
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid.c_str(), wifi_pass.c_str()); 
-  Serial.println("Connecting...");
+  WiFi.begin(ssid.c_str(), wifi_pass.c_str());
   int wifi_count = 0;
- //Wait 5 second. If device cannot connect with client mode, output status.
+  // Wait 5 second. If device cannot connect with client mode, output status.
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.println("Connecting...");
     wifi_count++;
     if (wifi_count > 10)
     {
@@ -497,16 +480,14 @@ void setup()
       break;
     }
   }
- 
-  //If device can connect with client mode, output status.
+
+  // If device can connect with client mode, output status.
   if (WiFi.status() == WL_CONNECTED)
   {
     WiFi.config(ip, gateway, subnet, dns);
-    Serial.println("Connected");
-    Serial.println(WiFi.localIP());
   }
- 
- // After connect as client mode, check IP address setting.
+
+  // After connect as client mode, check IP address setting.
   bool ping_gateway = Ping.ping(gateway, 2);
   bool ping_dns = Ping.ping(dns, 2);
   if (!ping_gateway)
@@ -532,10 +513,8 @@ void setup()
     }
   }
 
-
-
-//// Register http handler
- // Menu
+  //// Register http handler
+  // Menu
   server.on("/", HTTP_ANY, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/setup.html", String(), false, processor); });
 
@@ -548,7 +527,7 @@ void setup()
   server.on("/favicon", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/favicon.ico", "image/x-icon"); });
 
-  //// Download wifi setting 
+  //// Download wifi setting
   server.on("/wifi_config", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/wifi_config", "text/csv"); });
 
@@ -606,18 +585,16 @@ void setup()
         if (graph_sw == "2m"){ graph_duration = 470;
     request->send(SPIFFS, "/graph.html",String(),false,processor);}
         if (graph_sw == "/" || graph_sw == ""){
-    request->send(SPIFFS, "/setup.html",String(), false, processor);} 
-            });
+    request->send(SPIFFS, "/setup.html",String(), false, processor);} });
 
- // #2 O2sensor off/on
+  // #2 O2sensor off/on
   server.on("/secondary_sw", HTTP_ANY, [](AsyncWebServerRequest *request)
             {
     if(secondary_value == "OFF"){secondary_value = "ON";}
     else{secondary_value = "OFF";}
-    request->send(SPIFFS, "/graph.html",String(),false,processor);
-      });
+    request->send(SPIFFS, "/graph.html",String(),false,processor); });
 
- // Load graph engine.
+  // Load graph engine.
   server.on("/chart.jgz", HTTP_GET, [](AsyncWebServerRequest *request)
             { AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/chart.jgz", "text/javascript");
     response->addHeader("Content-Encoding", "gzip");
@@ -627,7 +604,7 @@ void setup()
             { AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/style.css", "text/css");
     request->send(response); });
 
- // Return sensor value
+  // Return sensor value
   server.on("/o2_in1", HTTP_GET, [](AsyncWebServerRequest *request)
             {  String o2_in1_str = String(in_volt1 ) ;
         request->send_P(200, "text/plain", o2_in1_str.c_str()); });
@@ -644,8 +621,8 @@ void setup()
       { if(secondary_value == "ON"){
               String o2_out2_str = String(serial_out_volt2) ;
               request->send_P(200, "text/plain", o2_out2_str.c_str()); } });
- 
-  // Display wifi setting 
+
+  // Display wifi setting
   server.on("/set-wifi", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/set-wifi.html", String(), false, processor); });
 
@@ -684,38 +661,39 @@ void setup()
 
   // Start software uploader
   AsyncElegantOTA.begin(&server);
-  // Start Webserial 
+  // Start Webserial
   WebSerial.begin(&server);
-   // http server start
- server.begin();
+  // http server start
+  server.begin();
 }
 
 void loop()
 {
- // Input sensor data from ADC.
-  int o2_value1 = ReadVoltage(O2IN_PIN1) * 1000;
-  int o2_value2 = ReadVoltage(O2IN_PIN2) * 1000;
+  // Input sensor data from ADC.
+  uint32_t o2_value1;
+  uint32_t o2_value2;
+  esp_adc_cal_get_voltage(ADC_CHANNEL_7, &adcChar, &o2_value1); // GPIO35
+  esp_adc_cal_get_voltage(ADC_CHANNEL_4, &adcChar, &o2_value2); // GPIO32
 
- // This value need to adjust due to ESP32 indivisuality.
- // An offset around 0v is added to adjust the slope between the actual and measured values.
- // In many cases, the error can be corrected simply by adjusting the offset.
- // Moving averages are taken to reduce sensor variability.
-  in_volt1 =  (in_volt1 * 5 + o2_value1) / 6  ;
-  in_volt2 =  (in_volt2 * 5 + o2_value2) / 6  ;
+  in_volt1 = (in_volt1 * 5 + o2_value1) / 6;
+  in_volt2 = (in_volt2 * 5 + o2_value2) / 6;
 
   serial_out_volt1 = in_volt1 + shift_value;
   serial_out_volt2 = in_volt2 + shift_value;
- // This value need to adjust due to ESP32 indivisuality.
- // An offset around 0v is added to adjust the slope between the actual and measured values.
- // In many cases, the error can be corrected simply by adjusting the offset.
- // Offset 10 10mv          Scale 35 10mv@ 800
-  int out_duty1 = (in_volt1 + shift_value - 70) * 255 / 3110;
-  int out_duty2 = (in_volt2 + shift_value - 60) * 255 / 3090;
- //                                         ^           ^
+
+  // This value need to adjust due to ESP32 indivisuality.
+  // An offset around 0v is added to adjust the slope between the actual and measured values.
+  // In many cases, the error can be corrected simply by adjusting the offset.
+  //  int out_duty1 = (in_volt1 + shift_value - 90) * 255 / 3070;
+  //  int out_duty2 = (in_volt2 + shift_value - 110) * 255 / 3110;
+  int out_duty1 = round((in_volt1 + shift_value - 100) * 0.0830);
+  int out_duty2 = round((in_volt2 + shift_value - 100) * 0.0830);
+
+  //                                         ^           ^
   if (af_value == "0.01")
   {
-    out_duty1 = 31;
-    out_duty2 = 32;
+    out_duty1 = 27;
+    out_duty2 = 28;
     serial_out_volt1 = 450;
     serial_out_volt2 = 450;
   }
@@ -734,7 +712,7 @@ void loop()
     if (serial_out_volt2 < 150)
     {
       serial_out_volt2 = 100;
-       out_duty2 = 1;
+      out_duty2 = 1;
     }
     if (serial_out_volt2 > 900)
     {
@@ -742,8 +720,11 @@ void loop()
       out_duty2 = 68;
     }
   }
-  dacWrite(O2OUT_PIN1, out_duty1);
-  dacWrite(O2OUT_PIN2, out_duty2);
+  //  dacWrite(O2OUT_PIN1, out_duty1);
+  //  dacWrite(O2OUT_PIN2, out_duty2);
+
+  dac_output_voltage(DAC_CHANNEL_1, out_duty1);
+  dac_output_voltage(DAC_CHANNEL_2, out_duty2);
 
   // Write log
   if (counter >= 10 && log_sw == "ON")
@@ -765,18 +746,33 @@ void loop()
     fileToAppend.close();
 
     // Write Web Serial
-    String web_str = String(o2_value1);
-    web_str += ",";
-    web_str += String(o2_value1);
-    web_str += ",";
-    web_str += String(out_duty1);
-    web_str += ",";
-    web_str += String(out_duty2);
-    web_str += ",";
-    web_str += (secondary_value);
-    //  web_str += ",";
-    // web_str += (secondary_sw);
-    WebSerial.println(web_str);
+
+    WebSerial.print("eFuse Vref:%u mV:");
+    WebSerial.print(adcChar.vref);
+    WebSerial.print(",");
+    WebSerial.print("coeff_a: ");
+    WebSerial.print(adcChar.coeff_a);
+    WebSerial.print(",");
+    WebSerial.print("coeff_b: ");
+    WebSerial.print(adcChar.coeff_b);
+    WebSerial.print("CPU_temp: ");
+    WebSerial.println(temperatureRead());
+
+    /*   String web_str = String(o2_value1);
+       web_str += ",";
+       web_str += (temperatureRead());
+       web_str += ",";
+        web_str += (getCpuFrequencyMhz());
+       web_str += String(o2_value1);
+       web_str += ",";
+       web_str += String(out_duty1);
+       web_str += ",";
+       web_str += String(out_duty2);
+       web_str += ",";
+       web_str += (secondary_value);
+       web_str += ",";
+      web_str += (secondary_sw);
+    WebSerial.println(web_str); */
     counter = 0;
   }
   else
